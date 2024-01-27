f6b8950283366bfb3cad41d515cc66f061dffb72
DaylightNebula <35437205+DaylightNebula@users.noreply.github.com>
2024-01-26 21:30:22 -0700

    in singleplayer mode, shutdown after 2 minutes if no player connected

--- Cubic-Plugin/src/main/kotlin/daylightnebula/cubic/paper/CubicPaper.kt
+++ Cubic-Plugin/src/main/kotlin/daylightnebula/cubic/paper/CubicPaper.kt
@@ -6,6 +6,7 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerQuitEvent
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitRunnable
 
 class CubicPaper: JavaPlugin(), Listener {
     private val singleplayerPort = System.getProperty("singleplayer")?.toInt()
@@ -15,11 +16,22 @@ class CubicPaper: JavaPlugin(), Listener {
         PaperCommunicator.start(getEmptyPort())
     }
 
-    // on enable, if single player port was given, send ready request to that port
     override fun onEnable() {
+        // register this as an event listener
         Bukkit.getPluginManager().registerEvents(this, this)
-        if (singleplayerPort != null)
+
+        // if single player port was given
+        if (singleplayerPort != null) {
+            // send ready request to that port
             PaperCommunicator.ready("localhost:$singleplayerPort")
+
+            // after 2 minutes, if no player connected, stop
+            Bukkit.getScheduler().runTaskLater(
+                this,
+                Runnable { if (Bukkit.getOnlinePlayers().isEmpty()) Bukkit.shutdown() },
+                2400
+            )
+        }
     }
 
     // on disable, stop communicator
